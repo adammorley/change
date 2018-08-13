72c124b351c9c5f5a3d2519d5a1adaa3b9613f6e
Adam Morley <adam@Adams-Mac-mini.local>
2018-08-13 13:14:56 -0700

    update comments on change thing

--- change.go
+++ change.go
@@ -6,15 +6,18 @@ package change
 
 import "math"
 
+var minCoins = make(map[int]int)
+var coinsUsed = make(map[int]map[int]int)
+
 /*
-    iterate through the coins available, using previously calculated minumum numbers to calculate the next number
+    iterate through the coins available, using previously calculated minumum numbers to calculate the next number.  uses a package variable.
 */
 func CalculateChange(amount int, coins []int) (int, map[int]int) {
-    var minCoins = make(map[int]int)
+    if numCoins, ok := minCoins[amount]; ok {
+        return numCoins, coinsUsed[amount]
+    }
     minCoins[0] = 0
-    var coinsUsed = make(map[int]map[int]int)
     coinsUsed[0] = make(map[int]int)
-
     for amt := 1; amt <= amount; amt++ {
         minCoins[amt] = math.MaxUint32
         for i := range coins {
@@ -32,4 +35,29 @@ func CalculateChange(amount int, coins []int) (int, map[int]int) {
         }
     }
     return minCoins[amount], coinsUsed[amount]
+}
+
+/*
+    accept requests on channels, send out responses based on package state.  run inside a goroutine. example:
+
+    var amount chan int = make(chan int)
+
+    var coins chan []int = make(chan []int)
+
+    var numCoins chan int = make(chan int)
+
+    var coinsUsed chan map[int]int = make(chan map[int]int)
+
+    go change.PersistentChangeCalculator(amount, coins, numCoins, coinsUsed)
+
+    amount <- 50
+
+    coins <- []int{25, 10, 5, 1}
+
+    fmt.Println(<-numCoins, <-coinsUsed)
+*/
+func PersistentChangeCalculator(amount <-chan int, coins <-chan []int, numCoins chan<- int, coinsUsed chan<- map[int]int) {
+    n, c := CalculateChange(<-amount, <-coins)
+    numCoins <- n
+    coinsUsed <- c
 }

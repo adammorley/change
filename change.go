7b8e526eb6cc7447e0a808ff3c4e65387fb75df5
Adam Morley <adam@Adams-Mac-mini.local>
2018-08-15 09:47:08 -0700

    gofmt

--- change.go
+++ change.go
@@ -1,5 +1,5 @@
 /*
-    calculate the number of coins (and which coins) are needed to make change for a given amount
+   calculate the number of coins (and which coins) are needed to make change for a given amount
 */
 
 package change
@@ -10,54 +10,54 @@ var minCoins = make(map[int]int)
 var coinsUsed = make(map[int]map[int]int)
 
 /*
-    iterate through the coins available, using previously calculated minumum numbers to calculate the next number.  uses a package variable.
+   iterate through the coins available, using previously calculated minumum numbers to calculate the next number.  uses a package variable.
 */
 func CalculateChange(amount int, coins []int) (int, map[int]int) {
-    if numCoins, ok := minCoins[amount]; ok {
-        return numCoins, coinsUsed[amount]
-    }
-    minCoins[0] = 0
-    coinsUsed[0] = make(map[int]int)
-    for amt := 1; amt <= amount; amt++ {
-        minCoins[amt] = math.MaxUint32
-        for i := range coins {
-            coin := coins[i]
-            if amt >= coin {
-                if 1 + minCoins[amt - coin] < minCoins[amt] {
-                    minCoins[amt] = 1 + minCoins[amt - coin]
-                    coinsUsed[amt] = make(map[int]int)
-                    for k, v := range coinsUsed[amt-coin] {
-                        coinsUsed[amt][k] = v
-                    }
-                    coinsUsed[amt][coin]++
-                }
-            }
-        }
-    }
-    return minCoins[amount], coinsUsed[amount]
+	if numCoins, ok := minCoins[amount]; ok {
+		return numCoins, coinsUsed[amount]
+	}
+	minCoins[0] = 0
+	coinsUsed[0] = make(map[int]int)
+	for amt := 1; amt <= amount; amt++ {
+		minCoins[amt] = math.MaxUint32
+		for i := range coins {
+			coin := coins[i]
+			if amt >= coin {
+				if 1+minCoins[amt-coin] < minCoins[amt] {
+					minCoins[amt] = 1 + minCoins[amt-coin]
+					coinsUsed[amt] = make(map[int]int)
+					for k, v := range coinsUsed[amt-coin] {
+						coinsUsed[amt][k] = v
+					}
+					coinsUsed[amt][coin]++
+				}
+			}
+		}
+	}
+	return minCoins[amount], coinsUsed[amount]
 }
 
 /*
-    accept requests on channels, send out responses based on package state.  run inside a goroutine. example:
+   accept requests on channels, send out responses based on package state.  run inside a goroutine. example:
 
-    var amount chan int = make(chan int)
+   var amount chan int = make(chan int)
 
-    var coins chan []int = make(chan []int)
+   var coins chan []int = make(chan []int)
 
-    var numCoins chan int = make(chan int)
+   var numCoins chan int = make(chan int)
 
-    var coinsUsed chan map[int]int = make(chan map[int]int)
+   var coinsUsed chan map[int]int = make(chan map[int]int)
 
-    go change.PersistentChangeCalculator(amount, coins, numCoins, coinsUsed)
+   go change.PersistentChangeCalculator(amount, coins, numCoins, coinsUsed)
 
-    amount <- 50
+   amount <- 50
 
-    coins <- []int{25, 10, 5, 1}
+   coins <- []int{25, 10, 5, 1}
 
-    fmt.Println(<-numCoins, <-coinsUsed)
+   fmt.Println(<-numCoins, <-coinsUsed)
 */
 func PersistentChangeCalculator(amount <-chan int, coins <-chan []int, numCoins chan<- int, coinsUsed chan<- map[int]int) {
-    n, c := CalculateChange(<-amount, <-coins)
-    numCoins <- n
-    coinsUsed <- c
+	n, c := CalculateChange(<-amount, <-coins)
+	numCoins <- n
+	coinsUsed <- c
 }
--- change_test.go
+++ change_test.go
@@ -4,9 +4,9 @@ import "reflect"
 import "testing"
 
 func TestChange(t *testing.T) {
-    testCase(t, 1, 67, []int{25, 10, 5, 1}, 6, map[int]int{25: 2, 10: 1, 5: 1, 1: 2})
-    testCase(t, 2, 67, []int{30, 5, 1}, 5, map[int]int{30: 2, 5: 1, 1: 2})
-    testCase(t, 3, 40, []int{25, 20, 10, 1}, 2, map[int]int{20: 2})
+	testCase(t, 1, 67, []int{25, 10, 5, 1}, 6, map[int]int{25: 2, 10: 1, 5: 1, 1: 2})
+	testCase(t, 2, 67, []int{30, 5, 1}, 5, map[int]int{30: 2, 5: 1, 1: 2})
+	testCase(t, 3, 40, []int{25, 20, 10, 1}, 2, map[int]int{20: 2})
 }
 
 // a test case for the change code
@@ -16,17 +16,17 @@ func TestChange(t *testing.T) {
 // the total number of coins returned by the function
 // the coins which should be returned by the function
 func testCase(t *testing.T, testCase int, amount int, coins []int, totalCoins int, validCoins map[int]int) {
-    var numCoins int
-    var coinsUsed map[int]int
-    numCoins, coinsUsed = CalculateChange(amount, coins)
+	var numCoins int
+	var coinsUsed map[int]int
+	numCoins, coinsUsed = CalculateChange(amount, coins)
 
-    if numCoins != totalCoins {
-        t.Error("number of coins wrong for test case", testCase)
-    } else if coinsDontMatch(coinsUsed, validCoins) {
-        t.Error("coins don't match for test case", testCase)
-    }
+	if numCoins != totalCoins {
+		t.Error("number of coins wrong for test case", testCase)
+	} else if coinsDontMatch(coinsUsed, validCoins) {
+		t.Error("coins don't match for test case", testCase)
+	}
 }
 
 func coinsDontMatch(calculated map[int]int, valid map[int]int) bool {
-    return !reflect.DeepEqual(calculated, valid)
+	return !reflect.DeepEqual(calculated, valid)
 }
